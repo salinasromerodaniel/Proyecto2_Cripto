5acf409a990d98dd950f9b583c5d727062550ff0
salinasromerodaniel <42517271+salinasromerodaniel@users.noreply.github.com>
2023-06-10 13:21:46 -0600

    Update Conexion_Algorand.py

--- Conexion_Algorand.py
+++ Conexion_Algorand.py
@@ -1,12 +1,63 @@
-import algosdk.v2client.algod as algod
-import ipfshttpclient
+from algosdk import account, algod, mnemonic
+from algosdk.future.transaction import AssetConfigTxn, AssetTransferTxn
+from algosdk.v2client import algod
+import hashlib
 
-algod_client = algod.AlgodClient(
+
+# Configuración del cliente de Algod
+client = algod.AlgodClient(
     algod_token="",
     algod_address="https://testnet-algorand.api.purestake.io/ps2",
-    headers={"X-API-Key": "pMVxEd580O2alV37M9fXD37jWdasSRPE2jTEj1xv"}
+    headers={"X-API-Key": "6cvGrbkG7J0CAdxAzHZN6pRqLLodXql5LbCTGYQi"}
 )
-#status = algod_client.status()
-#print(status)
-client = ipfshttpclient.connect("/ip4/127.0.0.1/tcp/5001")  # Establece la conexión con el nodo local de IPFS
-print(client)
+
+
+# Generar una cuenta para el propietario del token
+address = "FIMYONVZTTVTB5OWG2O7HZZWRCLJEUWDVDBJEBOT2Z223LOEOG47QLPIXE"
+private_key = mnemonic.to_private_key("plunge used oval copy access lecture apple black never false gossip ski area true velvet tomato hazard yellow ostrich topple journey kind arch abstract picture")
+
+
+# Configurar los parámetros de la transacción
+params = client.suggested_params()
+
+
+# Parámetros del token
+asset_name = "Apunte"
+asset_unitname = "DOC"
+asset_total = 1  # Como es un NFT, solo se crea una unidad
+asset_decimals = 0  # Nuevamente, como es un NFT, no hay decimales
+# En asset_url puedes poner el hash de IPFS del documento que quieres demostrar propiedad
+asset_url = "https://ipfs.io/ipfs/QmYourHash"
+metadata= "Doc Title: My Document"
+asset_metadata = hashlib.sha256(metadata.encode()).digest()
+
+
+
+
+# Crear la transacción
+txn = AssetConfigTxn(
+    sender=address,
+    sp=params,
+    total=asset_total,
+    default_frozen=False,
+    unit_name=asset_unitname,
+    asset_name=asset_name,
+    manager=address,
+    reserve=address,
+    freeze=address,
+    clawback=address,
+    url=asset_url,
+    metadata_hash=asset_metadata,
+    decimals=asset_decimals,
+)
+
+
+# Firmar la transacción
+stxn = txn.sign(private_key)
+
+
+# Enviar la transacción
+txid = client.send_transaction(stxn)
+print("Successfully sent transaction with txID: {}".format(txid))
+
+
